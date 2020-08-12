2048dafe50455378174ee551f71f521fd8c00b05
Ben Marvell <madmarv187@gmail.com>
2020-08-12 20:13:52 +0100

    Save and return

--- models/knex/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const knex = require('knex');
-const knexfile = require('./knexfile');
-const env = process.env.NODE_ENV || 'production';
-const options = knexfile[env];
-
-module.exports = knex(options);
